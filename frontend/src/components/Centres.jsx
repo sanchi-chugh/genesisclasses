80311aa94ea4df6172cacadc48037d565f34cca9
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2018-07-30 09:38:43 +0530

    Indent Centres.jsx and Courses.jsx properly

--- frontend/src/components/Centres.jsx
+++ frontend/src/components/Centres.jsx
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-
-// react table
 import ReactTable from 'react-table';
 import "react-table/react-table.css";
-
 import axios from 'axios';
-//import Hello from './Hello';
-//import './style.css';
 
 class Centres extends Component {
   constructor() {
@@ -19,42 +13,43 @@ class Centres extends Component {
   }
 
   textFilter(filter, row){
-    let result =parseInt( row[filter.id].toUpperCase().indexOf(filter.value.toUpperCase()), 10);
-    if(result < 0){
+    let result = parseInt( row[filter.id].toUpperCase().indexOf(filter.value.toUpperCase()), 10);
+    if (result < 0) {
       return false;
     }else{
       return true;
     }
   }
 
-componentDidMount() {
- axios.get("http://127.0.0.1:8000/api/centre/").then(res => {
-  const data = res.data;
-  this.setState({data});
- })
-}
+  componentDidMount() {
+    axios.get("/api/centres/").then(res => {
+      const data = res.data;
+      this.setState({data});
+    });
+  }
 
   render() {
     const columns = [
+      {
+        Header: 'Centres',
+        columns:[
           {
-            Header: 'Centres',
-            columns:[
-              {
-                accessor: 'location',
-                filterMethod: (filter,row) => {return this.textFilter(filter,row)}
-              }
-            ]
+            accessor: 'location',
+            filterMethod: (filter,row) => {return this.textFilter(filter,row)}
           }
-      ]
+        ]
+      }
+    ]
     return (
       <div>
         <ReactTable
-        data={this.state.data}
-        columns={columns}
-        noDataText="No Data Available"
-        filterable
-        defaultPageSize={5}
-        className="-striped -highlight" />
+          data={this.state.data}
+          columns={columns}
+          noDataText="No Data Available"
+          filterable
+          defaultPageSize={5}
+          className="-striped -highlight"
+        />
       </div>
     );
   }

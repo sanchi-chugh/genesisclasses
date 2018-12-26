4cfde3c40dff909d40243471edb62c4a47e3ada5
pareksha <pareksha.manchanda@gmail.com>
2018-12-26 13:24:52 +0530

    Resolve student creation admin dashboard bug
    When user with user type student is created,
    student obj is automatically created with a default
    centre val. Same for staff (default super admin val).

--- api/signals.py
+++ api/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from api.models import SuperAdmin, Staff, Student, User
+from api.models import SuperAdmin, Staff, Student, User, Centre
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, **kwargs):
@@ -11,11 +11,15 @@ def create_user_profile(sender, instance, **kwargs):
         if(profile[1]):
             profile[0].institution_name = instance.username
     elif instance.type_of_user == 'staff':
-        profile = Staff.objects.get_or_create(user=instance)
+        # Set a default superadmin, so that creation of obj from django admin dashboard is possible
+        super_admin = SuperAdmin.objects.all().order_by('pk')[0]
+        profile = Staff.objects.get_or_create(user=instance, super_admin=super_admin)
         if(profile[1]):
             profile[0].name = instance.username
     elif instance.type_of_user == 'student':
-        profile = Student.objects.get_or_create(user=instance)
+        # Set a default centre, so that creation of obj from django admin dashboard is possible
+        centre = Centre.objects.all().order_by('pk')[0]
+        profile = Student.objects.get_or_create(user=instance, centre=centre)
         if(profile[1]):
             profile[0].first_name = instance.username
     else:

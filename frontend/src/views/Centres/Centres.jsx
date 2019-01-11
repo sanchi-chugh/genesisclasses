553d8ea034b45141ddb465efb9c45ad18f3f11a1
Mohit Nagpal <nagpalm7@gmail.com>
2019-01-11 14:24:17 +0530

    render table

--- frontend/src/views/Centres/Centres.jsx
+++ frontend/src/views/Centres/Centres.jsx
@@ -3,7 +3,9 @@ import { Grid, Row, Col, Table } from "react-bootstrap";
 import axios from 'axios';
 
 import Card from "../../components/Card/Card.jsx";
-import Tables from "../../components/Table/Table";
+import {BootstrapTable, TableHeaderColumn, InsertModalFooter, InsertModalHeader, DeleteButton} from 'react-bootstrap-table';
+
+import "../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css";
 
 class Centres extends Component {
 
@@ -20,18 +22,78 @@ class Centres extends Component {
         Authorization: `Token ${localStorage.token}`
         }
     }).then(res => {
-        console.log(res.data);
-        const data = res.data;
+          const data = res.data.map(item => {
+          item.sno = res.data.indexOf(item) + 1;
+          return item;
+        })
         this.setState({data});
     });
   }
 
-  update(){
+  handleDelete = (onClick) => {
+    // Custom your onClick event here,
+    // it's not necessary to implement this function if you have no any process before onClick
+    console.log('This is my custom function for DeleteButton click event');
+    onClick();
     this.setState({
-      data :this.state.data
+      data : [{
+        sno:11,
+        location:'chandigarh'
+      }]
+    })
+  } 
+
+  handleSave(save) {
+    // Custom your onSave event here,
+    // it's not necessary to implement this function if you have no any process before save
+    this.props.update();
+    this.setState({
+      data : [{
+        sno:12,
+        location:'chandigarh'
+      }]
     })
   }
+
+  createCustomModalHeader = (closeModal, save) => {
+    return (
+      <InsertModalHeader
+        className='my-custom-class'
+        title='This is my custom title'
+        onModalClose={ () => this.handleModalClose(closeModal) }/>
+        // hideClose={ true } to hide the close button
+    );
+  }
+
+  createCustomModalFooter = (closeModal, save) => {
+    return (
+      <InsertModalFooter
+        className='my-custom-class'
+        saveBtnText='CustomSaveText'
+        closeBtnText='CustomCloseText'
+        closeBtnContextual='btn-warning'
+        saveBtnContextual='btn-success'
+        closeBtnClass='my-close-btn-class'
+        saveBtnClass='my-save-btn-class'
+        onSave={ () => this.handleSave(save) }/>
+    );
+  }
+
+  createCustomDeleteButton = (onClick) => {
+    return (
+      <DeleteButton
+        onClick={ e => this.handleDelete(onClick) }/>
+    );
+  }
+
   render() {
+
+    const options = {
+        insertModalHeader: this.createCustomModalHeader,
+        insertModalFooter: this.createCustomModalFooter,
+        deleteBtn: this.createCustomDeleteButton
+    };
+
     return (
       <div className="content">
         <Grid fluid>
@@ -42,7 +104,19 @@ class Centres extends Component {
                 ctTableFullWidth
                 ctTableResponsive
                 content={
-                  <Tables data={this.state.data} update={this.update.bind(this)}/>
+                  <div style={{margin:10}}>
+                    <BootstrapTable
+                      condensed pagination
+                      data={this.state.data}
+                      remote
+                      options={ options }
+                      insertRow={ true }
+                      search={ true }
+                      deleteRow={ true }>
+                        <TableHeaderColumn dataField='sno' isKey hiddenOnInsert>SNO.</TableHeaderColumn>
+                        <TableHeaderColumn dataField='location'>Centre</TableHeaderColumn>
+                    </BootstrapTable>
+                  </div>
                 }
               />
             </Col>

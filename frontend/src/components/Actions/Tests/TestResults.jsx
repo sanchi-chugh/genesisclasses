b4b6d462c1b0085eef99b4b0880a5647cc3b72c4
Mohit Nagpal <nagpalm7@gmail.com>
2019-05-27 21:30:49 +0530

    test results

--- frontend/src/components/Actions/Tests/TestResults.jsx
+++ frontend/src/components/Actions/Tests/TestResults.jsx
@@ -13,6 +13,8 @@ import {
 import axios from 'axios';
 import Button from "../../../components/CustomButton/CustomButton.jsx";
 import LinearProgress from '@material-ui/core/LinearProgress';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { VictoryStack, VictoryChart, VictoryGroup, VictoryBar } from 'victory';
 import { FormInputs } from "../../../components/FormInputs/FormInputs.jsx";
 
 import { Card } from "../../../components/Card/Card.jsx";
@@ -24,10 +26,12 @@ class TestResults extends Component {
     this.state = {
       data: [],
       busy:true,
+      flag: false,
+      page: 1,
       centres:[],
-      results:[],
+      results:{results:[]},
       formData:{
-        centre:null,
+        centre:0,
         start_date:null,
         end_date:null
       }
@@ -68,18 +72,33 @@ class TestResults extends Component {
     });
   }
 
-  handleViewResults(e){
-    e.preventDefault();
-    axios.get(`/api/results/tests/${this.props.match.params.id}/?start_date=${this.state.formData.start_date}&end_date=${this.state.formData.end_date}&centre=${this.state.formData.centre}`, {
+  handleViewResults(e=null, page,index=0){
+    if(e!== null){
+      e.preventDefault();
+    }
+    if(page===`/?page=1`){
+        page=""
+    }
+    axios.get(`/api/results/tests/${this.props.match.params.id}/?start_date=${this.state.formData.start_date}&end_date=${this.state.formData.end_date}&centre=${this.state.formData.centre}${page}`, {
         headers: {
         Authorization: `Token ${localStorage.token}`
         }
     }).then(res => {
-        const data = res.data.results;
+        const data = res.data;
         console.log(data)
         this.setState({
          results: data,
+         flag: data.results.length > 0,
       });
+    });
+  }
+
+  onPageChange(page, sizePerPage=10) {
+    const currentIndex = (page - 1) * sizePerPage;
+    this.handleViewResults(null, `/?page=${page}`,currentIndex);
+    console.log(currentIndex,page,sizePerPage,this.state.data);
+    this.setState({
+      page: page,
     });
   }
 
@@ -91,8 +110,123 @@ class TestResults extends Component {
   }
 
   render() {
+    const getBarData = () => {
+      return [1, 2, 3].map(() => {
+        return [
+          { x: 2001, y: Math.random() },
+          { x: 2002, y: Math.random() },
+          { x: 2003, y: Math.random() }
+        ];
+      });
+    };
     if(this.state.busy){
       return <div></div>;
+    }
+    if(this.state.flag){
+      return(
+          <div className="content">
+            <Grid fluid>
+              <Row>
+                <Col md={12}>
+                  <Card
+                    title="View Results"
+                    content={
+                      <Grid fluid>
+                        <form onSubmit={(event)=>this.handleViewResults(event,'/?page=1')}>
+                          <FormInputs
+                            ncols={["col-md-12"]}
+                            proprieties={[
+                              {
+                                label: "Start Date *",
+                                type: "date",
+                                bsClass: "form-control",
+                                name:'start_date',
+                                value:this.state.formData.start_date,
+                                // errors: errors,
+                                onChange:this.handleFormDataChange.bind(this)
+                              },
+                            ]}
+                          />
+                          <FormInputs
+                            ncols={["col-md-12"]}
+                            proprieties={[
+                              {
+                                label: "End Date *",
+                                type: "date",
+                                bsClass: "form-control",
+                                name:'end_date',
+                                value:this.state.formData.end_date,
+                                // errors: errors,
+                                onChange:this.handleFormDataChange.bind(this)
+                              },
+                            ]}
+                          />
+                            <FormGroup>
+                              <ControlLabel className='form-input'>Centre * </ControlLabel>
+                              <FormControl 
+                                componentClass="select" 
+                                value={this.state.formData.centre} 
+                                onChange={this.handleFormDataChange.bind(this)} 
+                                name="centre">
+                                  <option value='0'>All Centres</option>
+                                  { this.state.centres.map(item =>{
+                                    return(
+                                      <option value={item.id}>{item.location}</option>
+                                    )
+                                  })}
+                              </FormControl>  
+                              {
+                              //   Object.keys(errors)
+                              //           .some(item=> item === "centre") && 
+                              //               errors.centre.map(err=>
+                              //                   <HelpBlock>{err}</HelpBlock>
+                              //               )
+                              }
+                            </FormGroup>
+                          <LinearProgress
+                              style={
+                                  this.state.loadingResults ? 
+                                  {visibility: 'visible',marginBottom:10} :
+                                  {visibility: 'hidden'}
+                                  }
+                              color="primary"
+                              />
+                          <Button bsStyle="warning" pullRight fill type="submit">
+                            View Results
+                          </Button>
+                        <div className="clearfix" />
+                      </form>
+                      <div style={{marginTop:50}}>
+                        <BootstrapTable
+                          condensed pagination
+                          data={this.state.results.results}
+                          search remote
+                          fetchInfo={ { dataTotalSize: this.state.results.count } }
+                          options={ { sizePerPage: 10,
+                                      onPageChange: this.onPageChange.bind(this),
+                                      sizePerPageList: [ 10 ],
+                                      page: this.state.page} }>
+                            <TableHeaderColumn dataField='rank' isKey hiddenOnInsert>Rank</TableHeaderColumn>
+                            <TableHeaderColumn dataField='student.name'>Name</TableHeaderColumn>
+                            <TableHeaderColumn dataField='testAttemptDate'>Date</TableHeaderColumn>
+                            <TableHeaderColumn dataField='percentile'>Percentile</TableHeaderColumn>
+                            <TableHeaderColumn dataField='percentage'>Percentage</TableHeaderColumn>
+                            <TableHeaderColumn dataField='marksObtained'>Total Marks</TableHeaderColumn>
+                            <TableHeaderColumn dataField='marksObtained'>Marks Obtained</TableHeaderColumn>
+                            <TableHeaderColumn dataField='correct'>Total Questions</TableHeaderColumn>
+                            <TableHeaderColumn dataField='correct'>Correct</TableHeaderColumn>
+                            <TableHeaderColumn dataField='incorrect'>Incorrect</TableHeaderColumn>
+                            <TableHeaderColumn dataField='unattempted'>Unattempted</TableHeaderColumn>
+                        </BootstrapTable>
+                      </div>
+                      </Grid>
+                    }
+                  />
+                </Col>
+              </Row>
+            </Grid>
+      </div>
+        )
     }
     return (
       <div className="content">
@@ -103,7 +237,7 @@ class TestResults extends Component {
                 title="View Results"
                 content={
                   <Grid fluid>
-                    <form onSubmit={(event)=>this.handleViewResults(event)}>
+                    <form onSubmit={(event)=>this.handleViewResults(event, '/?page=1')}>
                       <FormInputs
                         ncols={["col-md-12"]}
                         proprieties={[
@@ -176,7 +310,40 @@ class TestResults extends Component {
                 title="Test Results"
                 content={
                   <Grid fluid>
-                    <p>Bar Graph</p>
+                    <div>
+                      <VictoryChart 
+                        domainPadding={{ x: 50 }} 
+                        width={400} 
+                        height={400}
+                        >
+                          <VictoryGroup offset={20} style={{ data: { width: 15 } }}>
+                            <VictoryStack  colorScale={["red", "#e5cd47", "green"]}>
+                              {getBarData().map((data, index) => {
+                                return <VictoryBar
+                                           key={index}
+                                           data={data}
+                                           />;
+                              })}
+                            </VictoryStack>
+                            <VictoryStack  colorScale={["red", "#e5cd47", "green"]}>
+                              {getBarData().map((data, index) => {
+                                return <VictoryBar
+                                           key={index}
+                                           data={data}
+                                           />;
+                              })}
+                            </VictoryStack>
+                            <VictoryStack  colorScale={["red", "#e5cd47", "green"]}>
+                              {getBarData().map((data, index) => {
+                                return <VictoryBar
+                                           key={index}
+                                           data={data}
+                                           />;
+                              })}
+                            </VictoryStack>
+                          </VictoryGroup>
+                        </VictoryChart>
+                    </div>
                   </Grid>
                 }
               />
